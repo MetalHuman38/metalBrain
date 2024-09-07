1e3ad6e055040ce927575157216a7eed0d27c67d
Babatunde Kalejaiye <kalejaib@roehampton.ac.uk>
2024-09-07 04:54:57 +0100

    fix eslint error

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,49 +1,112 @@
 const globals = require('globals');
 const reactRefresh = require('react-refresh');
-const typescriptParser = require('@typescript-eslint/parser')
-const pluginQuery = require('@tanstack/eslint-plugin-query')
+const typescriptParser = require('@typescript-eslint/parser');
+const pluginQuery = require('@tanstack/eslint-plugin-query');
 
-module.exports = {
-  languageOptions: {
-    ecmaVersion: 2022,
-    sourceType: "module",
-    globals: {
-      ...globals.browser,
-      ...globals.node,
-      myCustomGlobal: "readonly"
-    },
-    parser: typescriptParser,
-    ...pluginQuery.configs['flat/recommended'],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: "true",
-        tsx: "true",
-        ts: "true",
-        modules: "true",
+module.exports = [
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    ignores: [
+      "node_modules/**",
+      "build/**",
+      "dist/**",
+      "**/temp.js",
+      "config/*"
+    ],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        myCustomGlobal: "readonly"
       },
-      ecmaVersion: "latest",
-      sourceType: 'module',
-      tsconfigRootDir: __dirname, // ** This is the default value ** //
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+          tsx: true,
+        },
+        tsconfigRootDir: __dirname,
+      },
+    },
+    plugins: {
+      reactrefresh: reactRefresh,
+      pluginQuery: pluginQuery,
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+    },
+    rules: {
+      semi: ["warn", "always"],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
     },
   },
-  ignores: [
-    "node_modules/**",
-    "build/**",
-    "dist/**",
-    "**/temp.js",
-    "config/*"
-  ],
-  files: ["**/*.ts", "**/*.tsx"],
-  plugins: {
-    reactrefresh: reactRefresh,
-    '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+  {
+    files: ["**/*.js", "**/*.jsx"],
+    plugins: {
+      reactrefresh: reactRefresh,
+    },
+    rules: {
+      semi: ["warn", "always"],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
   },
-  rules: {
-    semi: ["warn", "always"],
-    "react-hooks/rules-of-hooks": "error",
-    // Enforces naming convention for hooks
-    "react-hooks/exhaustive-deps": "warn"
-  },
+];
 
 
-}
+
+// const globals = require('globals');
+// const reactRefresh = require('react-refresh');
+// const typescriptParser = require('@typescript-eslint/parser')
+// const pluginQuery = require('@tanstack/eslint-plugin-query')
+
+// module.exports = {
+//   languageOptions: {
+//     ecmaVersion: 2022,
+//     sourceType: "module",
+//     globals: {
+//       ...globals.browser,
+//       ...globals.node,
+//       myCustomGlobal: "readonly"
+//     },
+//     parser: typescriptParser,
+//     ...pluginQuery.configs['flat/recommended'],
+//     parserOptions: {
+//       ecmaFeatures: {
+//         jsx: "true",
+//         tsx: "true",
+//         ts: "true",
+//         modules: "true",
+//       },
+//       ecmaVersion: "latest",
+//       sourceType: 'module',
+//       tsconfigRootDir: __dirname, // ** This is the default value ** //
+//     },
+//   },
+//   ignores: [
+//     "node_modules/**",
+//     "build/**",
+//     "dist/**",
+//     "**/temp.js",
+//     "config/*"
+//   ],
+//   files: ["**/*.ts", "**/*.tsx"],
+//   plugins: {
+//     reactrefresh: reactRefresh,
+//     '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+//   },
+//   rules: {
+//     semi: ["warn", "always"],
+//     "react-hooks/rules-of-hooks": "error",
+//     // Enforces naming convention for hooks
+//     "react-hooks/exhaustive-deps": "warn"
+//   },
+
+
+// }
